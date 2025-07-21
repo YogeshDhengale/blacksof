17f6ffa41be50d5cc7cea20d3a04e3ad2f4e3463
YogeshDhengale <yd30668497@gmail.com>
2025-07-21 15:56:35 +0530

    Updating animation on small screen

--- components/PageComponents/ResponsiveView.tsx
+++ components/PageComponents/ResponsiveView.tsx
@@ -42,7 +42,7 @@ const ResponsiveView = () => {
                 hidden: { opacity: 0, y: 100, filter: "blur(4px)" },
                 visible: { opacity: 1, y: 0, filter: "blur(0px)" },
               }}
-              viewOptions={{ margin: "0px 0px -200px 0px" }}
+              viewOptions={{ margin: "0px 0px -50px 0px" }}
               transition={{ duration: 0.3, ease: "easeInOut" }}
             >
               <div>
